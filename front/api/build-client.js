26cea40578b0e3f20b2e9ed3e0b4de8746c8fd40
eloyk <stifler9.0@hotmail.com>
2021-01-09 12:37:21 -0400

    Prueba 11 para evitar error de consulta

--- front/api/build-client.js
+++ front/api/build-client.js
@@ -3,6 +3,8 @@ import axios from 'axios';
 export default ({ req }) => {
   if (typeof window === 'undefined') {
     // Peticion al servidor
+    console.log('ejecutando del lado del servidor');
+
     return axios.create({
       baseURL:
         'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
@@ -10,6 +12,8 @@ export default ({ req }) => {
     });
   } else {
     // Peticion al navegador
+    console.log('ejecutando del lado del servidor');
+
     return axios.create({
       baseUrl: '/',
     });
