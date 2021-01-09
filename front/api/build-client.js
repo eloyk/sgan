c9a38e3ad8fd1d2abea81bab62086796ea062fa8
eloyk <stifler9.0@hotmail.com>
2021-01-09 11:29:03 -0400

    Prueba 7 para evitar error de consulta

--- front/api/build-client.js
+++ front/api/build-client.js
@@ -6,7 +6,7 @@ export default ({ req }) => {
 
     return axios.create({
       baseURL:
-        'http://www.sgerd.com',
+        'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local',
       headers: req.headers,
     });
   } else {
