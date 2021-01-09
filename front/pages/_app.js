178678a80e2ee02353142e0b742c7d26acc7bf43
eloyk <stifler9.0@hotmail.com>
2021-01-09 01:37:09 -0400

    Update _app.js

--- front/pages/_app.js
+++ front/pages/_app.js
@@ -22,7 +22,7 @@ AppComponent.getInitialProps = async (appContext) => {
  
   const client = buildClient(appContext.ctx);
   //const { data } = await client.get('/api/usuario/usuarioactual');
-  let { data } = null;
+  let { data };
 
   let pageProps = {};
   if (appContext.Component.getInitialProps) {
