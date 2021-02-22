eaf112cf725cc8f4692b19a5053e1c40a489afee
eloyk <stifler9.0@hotmail.com>
2021-02-22 14:09:24 -0400

    prueba sin next otra 13

--- front/src/components/firebase/verifyCurrentUserPrueba.jsx
+++ front/src/components/firebase/verifyCurrentUserPrueba.jsx
@@ -1,40 +1,40 @@
-//import buildClient from "./build-client";
-import fetch from "isomorphic-unfetch"
-import absoluteUrl from "next-absolute-url"
+import buildClient from "./build-client";
+//import fetch from "isomorphic-unfetch"
+//import absoluteUrl from "next-absolute-url"
 
 async function verifyCurrentUserPrueba(ctx) {
-  const { origin } = absoluteUrl(ctx.req)
+  //const { origin } = absoluteUrl(ctx.req)
 
-  // Try to verify firebase token via API
-  return await fetch(origin + "/api/usuario/iniciarsesion", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({ 	
-      email: "test5@gmail.com",
-      password: "123456" 
-    })
-  }).then(res => {
-    if (res.ok) {
-      return res.json()
-    } else {
-      return false
-    }
-  })
-
-  // const client = buildClient(ctx);
-  // const { data } = await client
-  //   .post('/api/usuario/iniciarsesion',{
-  //     email,
-  //     password,
-  //   });
-  //   console.log('Este es el resultado de data: ' + data)
-  //   if (data) {
-  //     return data.json()
+  // // Try to verify firebase token via API
+  // return await fetch(origin + "/api/usuario/iniciarsesion", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   },
+  //   body: JSON.stringify({ 	
+  //     email: "test5@gmail.com",
+  //     password: "123456" 
+  //   })
+  // }).then(res => {
+  //   if (res.ok) {
+  //     return res.json()
   //   } else {
   //     return false
   //   }
+  // })
+
+  const client = buildClient(ctx);
+  const resp = await client
+    .post('/api/usuario/iniciarsesion',{
+      email: "test5@gmail.com",
+      password: "123456" 
+    });
+    console.log('Este es el resultado de data: ' + resp)
+    if (resp) {
+      return resp
+    } else {
+      return false
+    }
 
   //return data && data.usuarioActual
 }
