54fbca0cf6398ebfbbcf06728df4cd63b8c1a90a
eloyk <stifler9.0@hotmail.com>
2021-02-22 12:36:01 -0400

    prueba sin next otra 8

--- front/src/components/firebase/verifyCurrentUser.jsx
+++ front/src/components/firebase/verifyCurrentUser.jsx
@@ -3,14 +3,14 @@ import buildClient from "./build-client";
 async function verifyCurrentUser(ctx) {
 
   const client = buildClient(ctx);
-  return await client.get('/api/usuario/usuarioactual')
-  .then(res => {
-    if (res) {
-      return res.json()
-    } else {
-      return false
-    }
-  })
+  const { data } = await client.get('/api/usuario/usuarioactual');
+  console.log('Este es el resultado de data: ' + data)
+  if (data) {
+    return data
+  } else {
+    return false
+  }
+
   //return data && data.usuarioActual
 }
 
--- front/src/components/firebase/verifyCurrentUserPrueba.jsx
+++ front/src/components/firebase/verifyCurrentUserPrueba.jsx
@@ -1,25 +1,21 @@
 import buildClient from "./build-client";
 
 async function verifyCurrentUserPrueba(ctx, email, password) {
+
   const client = buildClient(ctx);
-  return await client.post('/api/usuario/iniciarsesion',{
-    email,
-    password,
-  })
-  .then(res => {
-    if (res) {
-      return res.json()
+  const { data } = await client
+    .post('/api/usuario/iniciarsesion',{
+      email,
+      password,
+    });
+    console.log('Este es el resultado de data: ' + data)
+    if (data) {
+      return data
     } else {
       return false
     }
-  })
-  // const client = buildClient(ctx);
-  // const { data } = await client
-  //   .post('/api/usuario/iniciarsesion',{
-  //     email,
-  //     password,
-  //   });
-  // return data && data.usuarioActual
+
+  //return data && data.usuarioActual
 }
 
 export default verifyCurrentUserPrueba
