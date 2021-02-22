16f980b754afdc5bf57a9fab7cc273e5e7b3788e
eloyk <stifler9.0@hotmail.com>
2021-02-22 12:55:24 -0400

    prueba sin next otra 9

--- front/src/components/firebase/verifyCurrentUser.jsx
+++ front/src/components/firebase/verifyCurrentUser.jsx
@@ -1,15 +1,32 @@
-import buildClient from "./build-client";
+//import buildClient from "./build-client";
+import fetch from "isomorphic-unfetch"
 
 async function verifyCurrentUser(ctx) {
+  const { origin } = absoluteUrl(ctx.req)
 
-  const client = buildClient(ctx);
-  const { data } = await client.get('/api/usuario/usuarioactual');
-  console.log('Este es el resultado de data: ' + data)
-  if (data) {
-    return data
-  } else {
-    return false
-  }
+  // Try to verify firebase token via API
+  return await fetch(origin + "/api/usuario/usuarioactual", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  }).then(res => {
+    if (res.ok) {
+      console.log('Este es el resultado del Current User: ' + res.json())
+      return res.json()
+    } else {
+      return false
+    }
+  })
+
+  // const client = buildClient(ctx);
+  // const { data } = await client.get('/api/usuario/usuarioactual');
+  // console.log('Este es el resultado de data: ' + data)
+  // if (data) {
+  //   return data
+  // } else {
+  //   return false
+  // }
 
   //return data && data.usuarioActual
 }
--- front/src/components/firebase/verifyCurrentUserPrueba.jsx
+++ front/src/components/firebase/verifyCurrentUserPrueba.jsx
@@ -1,19 +1,36 @@
 import buildClient from "./build-client";
 
 async function verifyCurrentUserPrueba(ctx, email, password) {
+  const { origin } = absoluteUrl(ctx.req)
 
-  const client = buildClient(ctx);
-  const { data } = await client
-    .post('/api/usuario/iniciarsesion',{
-      email,
-      password,
-    });
-    console.log('Este es el resultado de data: ' + data)
-    if (data) {
-      return data
+  // Try to verify firebase token via API
+  return await fetch(origin + "/api/usuario/iniciarsesion", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ email,password })
+  }).then(res => {
+    if (res.ok) {
+      console.log('Este es el resultado del login: ' + res.json())
+      return res.json()
     } else {
       return false
     }
+  })
+
+  // const client = buildClient(ctx);
+  // const { data } = await client
+  //   .post('/api/usuario/iniciarsesion',{
+  //     email,
+  //     password,
+  //   });
+  //   console.log('Este es el resultado de data: ' + data)
+  //   if (data) {
+  //     return data.json()
+  //   } else {
+  //     return false
+  //   }
 
   //return data && data.usuarioActual
 }
