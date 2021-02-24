ad6f672c7313f3b7c9574e682b17eec99289dbb2
eloyk <stifler9.0@hotmail.com>
2021-02-23 21:53:45 -0400

    verificacion de cambios en la autenticacion26

--- front/src/components/firebase/verifyCurrentUser.jsx
+++ front/src/components/firebase/verifyCurrentUser.jsx
@@ -5,8 +5,9 @@ async function verifyCurrentUser(ctx) {
   const client = buildClient(ctx);
   const resp = await client
     .get('/api/usuario/usuarioactual');
-    const data = JSON.parse(resp.data.usuarioActual)
-    console.log("DATA: " + data)
+    const data = JSON.stringify(resp.data.usuarioActual)
+    const result = JSON.parse(data)
+    console.log("DATA: " + data + "," + result)
     if (data != "null") {
       return data
     } else {
