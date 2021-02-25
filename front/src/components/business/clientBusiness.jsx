5e240109760c800d39e55e3d9675e5b585a2b9f0
eloyk <stifler9.0@hotmail.com>
2021-02-25 10:05:44 -0400

    modificacion y creacion de pantallas35

--- front/src/components/business/clientBusiness.jsx
+++ front/src/components/business/clientBusiness.jsx
@@ -2,7 +2,7 @@ import doRequest from "../api/do-request"
 import PAGE from "config/page.config"
 import Router from "next/router"
 
-const createBusiness = async ({
+const createBusiness = async ({props,
   nombreEmpresa, 
   clasifEmpresa, 
   tipoEmpresa, 
@@ -12,7 +12,7 @@ const createBusiness = async ({
   RNC, 
   usuarioIdAlta, 
   emailUsuarioAlta, 
-  }) => {
+  onSuccess}) => {
 
   await doRequest({
     url: '/api/empresa',
@@ -31,14 +31,14 @@ const createBusiness = async ({
   })
   .then(data => {
 
-    // if (onSuccess) {
-    //   onSuccess();
-    // } else {
-    //   () => Router.push(Router.query.redirect || PAGE.dashboardPagePath);
-    // }
+    if (onSuccess) {
+      onSuccess();
+    } else {
+      () => Router.push(Router.query.redirect || PAGE.dashboardPagePath);
+    }
     const convetData = JSON.stringify(data)
     console.log('Data proveniente del clientBusiness: ' + convetData)
-
+    props.currentBusinessChange(this.props.business)
     return data;
   })
   .catch(err => {
