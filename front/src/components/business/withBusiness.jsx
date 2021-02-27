a70ea3c8d2b11a802ca8c75660af2925d13ae735
eloyk <stifler9.0@hotmail.com>
2021-02-27 14:22:51 -0400

    modificacion y creacion de pantallas90

--- front/src/components/business/withBusiness.jsx
+++ front/src/components/business/withBusiness.jsx
@@ -37,7 +37,7 @@ function WithVerifyBusiness(AuthComponent) {
 
     componentDidMount() {
       console.log('Este es el valor del context:' + JSON.stringify(this.props.currentUser))
-      const result = await getBusiness(this.props.currentUser.empresaId)
+      const result = getBusiness(this.props.currentUser.empresaId)
       if (result) {
         this.props.currentBusinessChange(result)
           
