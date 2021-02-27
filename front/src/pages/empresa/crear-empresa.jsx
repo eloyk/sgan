35b322847b1c66039c9640bdbb802786eb17b8f3
eloyk <stifler9.0@hotmail.com>
2021-02-27 19:53:51 -0400

    modificacion y creacion de pantallas108

--- front/src/pages/empresa/crear-empresa.jsx
+++ front/src/pages/empresa/crear-empresa.jsx
@@ -120,7 +120,7 @@ function BusinessForm({props}) {
     // Apply Yup as resolver for react-hook-form
     resolver: yupResolver(schema),
     // Define the default values for all input forms
-    defaultValues:() => {
+    defaultValues:((props) => {
       const {
         nombreEmpresa, 
         clasifEmpresa, 
@@ -159,7 +159,7 @@ function BusinessForm({props}) {
 
       }
       return values
-  }
+  })(props)
   })
 
   // Handle form submit event
