fa99108ed2049f8571120c86c9347bddf17ca395
eloyk <stifler9.0@hotmail.com>
2021-02-27 12:07:06 -0400

    modificacion y creacion de pantallas76

--- front/src/components/business/withBusiness.jsx
+++ front/src/components/business/withBusiness.jsx
@@ -14,7 +14,7 @@ function WithVerifyBusiness(AuthComponent) {
       if (AuthComponent.getInitialProps) {
         initialProps = await AuthComponent.getInitialProps(ctx)
       }
-      console.log('Este es el valor del context:' + ctx)
+      console.log('Este es el valor del context:' + JSON.stringify(ctx))
       // Verify cookie
       //const result = await verifyCurrentBusiness(ctx)
 
