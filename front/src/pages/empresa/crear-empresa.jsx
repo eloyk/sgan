9b4b430616d67acf1a59dcdf90246bbc9b4d736a
eloyk <stifler9.0@hotmail.com>
2021-03-01 11:17:29 -0400

    modificacion y creacion de pantallas154

--- front/src/pages/empresa/crear-empresa.jsx
+++ front/src/pages/empresa/crear-empresa.jsx
@@ -345,7 +345,7 @@ function BusinessForm({props}) {
         </Col>
       </Row>
       <DemoWrapper>
-        <Button type="submit" variant="success">
+        <Button type="submit" disabled={defaultValues.flagDisabled} variant="success">
           {loading ? <Spinner className="mr-2" /> : null} Guardar
         </Button>{" "}
         <Button type="button" variant="warning">
